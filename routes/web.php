3078fb71656c7fc9ed56c5ff01f23b0a714ba994
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-12-16 21:52:41 +0600

    new custom middleware is created to check if the user is loggedin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MiddlewareController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckUserMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,10 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/private-message',[MiddlewareController::class,'privateMessage'])->name('user.message');
+    
 });
 
 //Controlling Download Through Middleware. 2 download in 1 minutes
 Route::get('/download',[MiddlewareController::class,'downloadFile'])->middleware(['auth','throttle:2,1']);
+
+//Using own made middileware to check if the user is loggedin
+Route::get('/private-message',[MiddlewareController::class,'privateMessage'])->middleware(CheckUserMiddleware::class);
 
 require __DIR__.'/auth.php';
