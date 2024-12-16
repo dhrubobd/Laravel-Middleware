f231c2e4027a59ad05f1105cd2e7025a8e7a196b
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-12-16 22:11:53 +0600

    Developer Credit Middleware is Created and Set As Globally which will show developer's name at the response header on all pages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\DeveloperCreditMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        //Setting Developer Credit Middleware Globally
+        $middleware->append(DeveloperCreditMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
